58b1579ce6b6388a7081a28ef2cea1b614571af3
marta.verdugo <marta.verdugo@nanoporetech.com>
2023-03-13 15:35:27 +0000

    correct timing

--- src/tired_MVERDUGO89/tired.py
+++ src/tired_MVERDUGO89/tired.py
@@ -1,16 +1,8 @@
 """ Send tired message whenever the time is 0 and 30 minutes past the hour."""
 import datetime
-import time
-
-time_now = datetime.datetime.now().time()
 
 
-def tired():
-    tired_message = "I'm tired, go away"
-    print(tired_message)
-
-
-while 00 <= time_now.minute < 30:
-    print(time_now.minute)
-    time.sleep(3)
-    tired()
+def check_tired():
+    time_now = datetime.datetime.now().time()
+    if 00 <= time_now.minute < 30:
+        return True
